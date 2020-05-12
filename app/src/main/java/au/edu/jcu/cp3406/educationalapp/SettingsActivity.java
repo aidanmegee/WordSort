d90f301b7aa925f01d0c5ae9f435abaca27f0640
Aidan <aidan.megee@my.jcu.edu.au>
2020-05-12 13:59:21 +1000

    Implement code for shake event to restart game activity. Check if device has accelerometer. Add lifecycle methods for Game Activity.

--- app/src/main/java/au/edu/jcu/cp3406/educationalapp/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/educationalapp/SettingsActivity.java
@@ -2,13 +2,26 @@ package au.edu.jcu.cp3406.educationalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button finishButton = findViewById(R.id.finished);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //Button listener for the user to return to the "Home Page"
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+            }
+        });
     }
 }
